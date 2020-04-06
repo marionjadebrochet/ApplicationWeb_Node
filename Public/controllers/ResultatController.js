5dd1a49d971514d949eb1443ba24ee5f3bf9c38c
GuillaumeRslt <guillaume.rousselet@etu.unilim.fr>
2020-04-06 10:01:38 +0200

    résultat (sans css)

--- Public/controllers/ResultatController.js
+++ Public/controllers/ResultatController.js
@@ -17,100 +17,121 @@ module.exports.ListerResultat = function(request, response){
 
 // ////////////////////// D E T A I l    N O M    P R E N O M    T E M P S
 
+// module.exports.DescResultat = 	function(request, response){
+// let data = request.params.nom;
+// async.parallel ([
+//   function (callback) {
+//     model.getListeRes( function (err, result) {
+//       callback(null, result) });
+//   },
+//   function(callback) {
+//     model.getPiloteTemps(data, function (err, result) {
+//       callback(null, result) });
+//   },
+// ],
+//   function (err, result){
+//     if (err) {
+//         // gestion de l'erreur
+//         console.log(err);
+//         return;
+//     }
+//     console.log(result);
+//     response.listeGrandPrix = result[0];
+//     response.piloteEtTemps = result[1];
+//     response.render('detailsResultat', response);
+//   }
+// ); //fin async
+// };
+//
+//
+// // ////////////////////// A J O U T    P L A C E
+//
+// module.exports.DescResultat = 	function(request, response){
+// let data = request.params.nom;
+// async.parallel ([
+//   function (callback) {
+//     model.getListeRes( function (err, result) {
+//       callback(null, result) });
+//   },
+//   function(callback) {
+//     model.getPiloteTemps(data, function (err, result) {
+//       callback(null, result) });
+//   },
+// 	function(callback) {
+// 		model.getPlacePilote(data, function (err, result) {
+// 			callback(null, result) });
+// 	},
+// ],
+//   function (err, result){
+//     if (err) {
+//         // gestion de l'erreur
+//         console.log(err);
+//         return;
+//     }
+//     console.log(result);
+//     response.listeGrandPrix = result[0];
+//     response.piloteEtTemps = result[1];
+// 		response.placeEtPoint = result[2];
+//     response.render('detailsResultat', response);
+//   }
+// ); //fin async
+// };
+//
+//
+// // ////////////////////// A J O U T    P L A C E    E T    D E T A I L
+//
+// module.exports.DescResultat = 	function(request, response){
+// let data = request.params.num;
+// async.parallel ([
+//   function (callback) {
+//     model.getListeRes( function (err, result) {
+//       callback(null, result) });
+//   },
+//   function(callback) {
+//     model.getPiloteTemps(data, function (err, result) {
+//       callback(null, result) });
+//   },
+// 	function(callback) {
+// 		model.getPlacePilote(data, function (err, result) {
+// 			callback(null, result) });
+// 	},
+// 	function(callback) {
+// 		model.getDescriptionGP(data, function (err, result) {
+// 			callback(null, result) });
+// 	},
+// ],
+//   function (err, result){
+//     if (err) {
+//         // gestion de l'erreur
+//         console.log(err);
+//         return;
+//     }
+//     console.log(result);
+//     response.listeGrandPrix = result[0];
+//     response.piloteEtTemps = result[1];
+// 		response.placeEtPoint = result[2];
+// 		response.details = result[3];
+//     response.render('detailsResultat', response);
+//   }
+// ); //fin async
+// };
+
 module.exports.DescResultat = 	function(request, response){
-let data = request.params.nom;
-async.parallel ([
-  function (callback) {
-    model.getListeRes( function (err, result) {
-      callback(null, result) });
-  },
-  function(callback) {
-    model.getPiloteTemps(data, function (err, result) {
-      callback(null, result) });
-  },
-],
-  function (err, result){
-    if (err) {
-        // gestion de l'erreur
-        console.log(err);
-        return;
-    }
-    console.log(result);
-    response.listeGrandPrix = result[0];
-    response.piloteEtTemps = result[1];
-    response.render('detailsResultat', response);
-  }
-); //fin async
-};
-
-
-// ////////////////////// A J O U T    P L A C E
-
-module.exports.DescResultat = 	function(request, response){
-let data = request.params.nom;
-async.parallel ([
-  function (callback) {
-    model.getListeRes( function (err, result) {
-      callback(null, result) });
-  },
-  function(callback) {
-    model.getPiloteTemps(data, function (err, result) {
-      callback(null, result) });
-  },
-	function(callback) {
-		model.getPlacePilote(data, function (err, result) {
-			callback(null, result) });
-	},
-],
-  function (err, result){
-    if (err) {
-        // gestion de l'erreur
-        console.log(err);
-        return;
-    }
-    console.log(result);
-    response.listeGrandPrix = result[0];
-    response.piloteEtTemps = result[1];
-		response.placeEtPoint = result[2];
-    response.render('detailsResultat', response);
-  }
-); //fin async
-};
-
-
-// ////////////////////// A J O U T    P L A C E    E T    D E T A I L
-
-module.exports.DescResultat = 	function(request, response){
+response.title = 'Liste des résulats des grands prix';
 let data = request.params.num;
-async.parallel ([
-  function (callback) {
-    model.getListeRes( function (err, result) {
-      callback(null, result) });
-  },
-  function(callback) {
-    model.getPiloteTemps(data, function (err, result) {
-      callback(null, result) });
-  },
-	function(callback) {
-		model.getPlacePilote(data, function (err, result) {
-			callback(null, result) });
-	},
-	function(callback) {
-		model.getDescriptionGP(data, function (err, result) {
-			callback(null, result) });
-	},
-],
-  function (err, result){
-    if (err) {
-        // gestion de l'erreur
-        console.log(err);
-        return;
-    }
-    console.log(result);
-    response.listeGrandPrix = result[0];
-    response.piloteEtTemps = result[1];
-		response.placeEtPoint = result[2];
-		response.details = result[3];
+
+model.getPiloteTemps(data, function (err, result) {
+		if (err) {
+				// gestion de l'erreur
+				console.log(err);
+				return;
+		}
+
+    response.piloteEtTemps = result
+		response.date = result[0].gpdate;
+		response.gpnom = result[0].gpnom;
+		response.detail = result[0].gpcommentaire;
+		;
     response.render('detailsResultat', response);
   }
 ); //fin async
--- Public/models/resultat.js
+++ Public/models/resultat.js
@@ -29,32 +29,51 @@ module.exports.getListeRes = function (callback) {
       });
 };
 
+// module.exports.getPiloteTemps = function (data, callback) {
+//     db.getConnection(function (err, connexion) {
+//         if (!err) {
+//             let sql = "SELECT gp.GPNUM as num, gp.GPNOM as nomGP, gp.GPDATE as date, p.PILNOM as nom, p.PILPRENOM as prenom, c.TEMPSCOURSE as temps FROM Pilote p LEFT JOIN course c ON c.PILNUM = p.PILNUM LEFT JOIN grandprix gp ON c.GPNUM = gp.GPNUM WHERE gp.GPNUM like \'" + data + "\' ORDER BY `temps` ASC ";
+//             connexion.query(sql, callback);
+//             connexion.release();
+//         }
+//     })
+// };
+//
+// module.exports.getPlacePilote = function (data, callback) {
+//     db.getConnection(function (err, connexion) {
+//         if (!err) {
+//             let sql = "SELECT PTPLACE as place, PTNBPOINTSPLACE as point FROM points";
+//             connexion.query(sql, callback);
+//             connexion.release();
+//         }
+//     })
+// };
+//
+// module.exports.getDescriptionGP = function (data, callback) {
+//     db.getConnection(function (err, connexion) {
+//         if (!err) {
+//             let sql = "SELECT GPCOMMENTAIRE as detail from grandprix WHERE GPNUM like \'" + data + "\'";
+//             connexion.query(sql, callback);
+//             connexion.release();
+//         }
+//     })
+// };
+
 module.exports.getPiloteTemps = function (data, callback) {
     db.getConnection(function (err, connexion) {
         if (!err) {
-            let sql = "SELECT gp.GPNUM as num, gp.GPNOM as nomGP, gp.GPDATE as date, p.PILNOM as nom, p.PILPRENOM as prenom, c.TEMPSCOURSE as temps FROM Pilote p LEFT JOIN course c ON c.PILNUM = p.PILNUM LEFT JOIN grandprix gp ON c.GPNUM = gp.GPNUM WHERE gp.GPNUM like \'" + data + "\' ORDER BY `temps` ASC ";
-            connexion.query(sql, callback);
-            connexion.release();
-        }
-    })
-};
+					let sql = "Select place, pilnom, tempscourse, ptnbpointsplace, gpnum, gpdate, gpnom, gpcommentaire, pilnum"
+							+ " from ("
+								+ " select @n := @n + 1 place, pilnom, tempscourse, gp.gpnum, gpdate, gpnom, gpcommentaire, p.pilnum"
+								+ " from pilote p join (SELECT @n := 0)t"
+								+ " JOIN course c ON c.pilnum = p.pilnum"
+								+ " JOIN grandprix gp ON c.gpnum = gp.gpnum"
+								+ " WHERE gp.gpnum =" + connexion.escape(data)
+								+ " order by tempscourse)tab"
+							+ " join points on points.PTPLACE=tab.place";
 
-module.exports.getPlacePilote = function (data, callback) {
-    db.getConnection(function (err, connexion) {
-        if (!err) {
-            let sql = "SELECT PTPLACE as place, PTNBPOINTSPLACE as point FROM points";
-            connexion.query(sql, callback);
-            connexion.release();
-        }
-    })
-};
-
-module.exports.getDescriptionGP = function (data, callback) {
-    db.getConnection(function (err, connexion) {
-        if (!err) {
-            let sql = "SELECT GPCOMMENTAIRE as detail from grandprix WHERE GPNUM like \'" + data + "\'";
-            connexion.query(sql, callback);
-            connexion.release();
+					connexion.query(sql, callback);
+					connexion.release();
         }
     })
 };
